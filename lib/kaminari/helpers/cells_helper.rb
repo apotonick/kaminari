9be90bcd653e979fa018a8502f3c7ada6ad42f6a
Nick Sutterer <apotonick@gmail.com>
2014-05-07 10:44:23 +1000

    add K:Helpers::CellsHelper.

---
+++ lib/kaminari/helpers/cells_helper.rb
@@ -0,0 +1,13 @@
+module Kaminari
+  module Helpers
+    module CellsHelper
+      include ActionViewExtension
+      include ActionView::Helpers::OutputSafetyHelper
+
+      def paginate(scope, options = {}, &block)
+        options = options.reverse_merge(:views_prefix => "../views/")
+        super
+      end
+    end
+  end
+end
